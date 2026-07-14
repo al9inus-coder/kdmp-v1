29f383caddc1361b3136e44e19a143cab9d74295
al9inus-coder <al9inus@gmail.com>
2026-07-14 23:10:56 +0700

    style: separate legend boxes into distinct cells for Hari Kerja and Hari Libur

--- resources/views/overtimes/print_rekap.blade.php
+++ resources/views/overtimes/print_rekap.blade.php
@@ -243,18 +243,17 @@
                 <tr>
                     <td style="border: none; padding: 0 5px 2px 0;">*)</td>
                     <td style="border: none; padding: 0 10px 2px 0;">Hari Bulan</td>
+                    <td style="border: none; width: 25px;"></td>
                 </tr>
                 <tr>
                     <td style="border: none; padding: 0 5px 2px 0;">**)</td>
                     <td style="border: none; padding: 0 10px 2px 0;">Hari Kerja</td>
-                    <td rowspan="2" style="border: 1px solid #000; width: 25px; vertical-align: bottom;">
-                        <div style="height: 15px; border-bottom: 1px solid #000; background-color: #fff;"></div>
-                        <div class="bg-holiday" style="height: 15px;"></div>
-                    </td>
+                    <td style="border: 1px solid #000; width: 25px; height: 12px; background-color: #ffffff;"></td>
                 </tr>
                 <tr>
                     <td style="border: none; padding: 0 5px 0 0;">***)</td>
                     <td style="border: none; padding: 0 10px 0 0;">Hari Libur</td>
+                    <td class="bg-holiday" style="border: 1px solid #000; width: 25px; height: 12px;"></td>
                 </tr>
             </table>
         </div>
